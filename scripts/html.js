5ffaf9f5637ec30ab4a595f07ebeb93d22ae3eb3
VINESA86 <vanebfer@icloud.com>
2022-10-19 16:28:07 +0200

    Feature Project3

--- scripts/html.js
+++ scripts/html.js
@@ -1,12 +1,43 @@
-console.log("hola mundo");
-export  const fetchJson = async () => {
+ const fetchJson = async () => {
+   
     const response =  await  fetch("https://api.propublica.org/congress/v1/113/house/members.json", {
 	method: "GET",
 	headers: {
     'X-API-KEY': 'g2lQIwb9v51mUxgD4lachz2DRzeIx04sUqEBcY14'
   }
 })
-.then(data=> data.json())
-.then(table => console.log(table))
-.catch(error=> console.log(error))
+.then(data => data.json())
+.then(table => {
+   let   members = table.results[0].members;
+    console.log(members);
+
+    let tbodyContent = "";
+    members.forEach(member => {
+    console.log(member);
+    let tr =`
+        <tr>
+        <td>${member.first_name} ${member.second_name} ${member.last_name}</td>
+        <td>${member.party}</td>
+        <td>${member.state}</td>
+        <td>${member.seniority}</td>
+        <td>${member.votes_with_party_pct}</td>
+    </tr>
+    `; 
+    tbodyContent += tr;
+    })
+
+    // dom senateId
+    document.getElementById("senateId").innerHTML= tbodyContent;
+})
+.catch(error=> console.log('ERR ',error))
 }
+
+fetchJson();
+
+
+
+
+//function to create a table 
+
+
+
